461d9d676dd6648dacd3112f8f60f106da4cd055
Zach Margolis <zbmargolis@gmail.com>
2023-12-12 18:03:13 -0800

    Basic loading site, eslint, prettier

--- src/app.tsx
+++ src/app.tsx
@@ -1,53 +1,90 @@
+import { useQuery } from "preact-fetching";
+
+import { csv } from "d3-fetch";
+
+interface Row {
+  timestamp: Date;
+  person: string;
+  denomination: number;
+  currency: string;
+}
+
+function loadCSV(): Promise<Row[]> {
+  return csv("pennies.csv").then((rows) =>
+    rows.map(({ timestamp, person, denomination, currency }) => ({
+      timestamp: new Date(+timestamp),
+      person,
+      denomination: +denomination,
+      currency,
+    }))
+  );
+}
+
 export function App() {
-  return <article>
-    <h1>
-      Pennies!
-      {" "}
-      <small>By <a href="/">Zach Margolis</a></small>
-    </h1>
+  const { isLoading, data } = useQuery("/pennies.csv", loadCSV);
 
-    <p>This is the change that my family and I have found in every year since 2017. It's our friendly little competition, and everyone's a winner (and I'm the biggest winner).</p>
+  return (
+    <article>
+      <h1>
+        Pennies!{" "}
+        <small>
+          By <a href="/">Zach Margolis</a>
+        </small>{" "}
+        {isLoading ? "..." : data?.length}
+      </h1>
 
-    <h2>
-      Results
-    </h2>
+      <p>
+        This is the change that my family and I have found in every year since 2017. It's our
+        friendly little competition, and everyone's a winner (and I'm the biggest winner).
+      </p>
 
-    <p>Total coins picked up, year-over-year</p>
+      <h2>Results</h2>
 
-    <div class="year-over-year overflow-x-scroll overflow-x-padding"></div>
+      <p>Total coins picked up, year-over-year</p>
 
-    <h2 class="sticky-header">
-      Year in Detail
-      <ul class="year-selector"></ul>
-    </h2>
+      <div className="year-over-year overflow-x-scroll overflow-x-padding"></div>
 
-    <div class="by-person"></div>
+      <h2 className="sticky-header">
+        Year in Detail
+        <ul className="year-selector"></ul>
+      </h2>
 
-    <h3>Timeline</h3>
-    <div class="by-time overflow-x-scroll overflow-x-padding"></div>
-    <ul class="coin-legend no-bullet"></ul>
+      <div className="by-person"></div>
 
-    <h3 class="clearfix">By Weekday</h3>
-    <p>Number of coins picked up by weekday</p>
-    <div class="by-weekday overflow-x-scroll overflow-x-padding"></div>
+      <h3>Timeline</h3>
+      <div className="by-time overflow-x-scroll overflow-x-padding"></div>
+      <ul className="coin-legend no-bullet"></ul>
 
-    <h3 class="clearfix">By Coin</h3>
-    <div class="by-coin-table overflow-x-scroll"></div>
+      <h3 className="clearfix">By Weekday</h3>
+      <p>Number of coins picked up by weekday</p>
+      <div className="by-weekday overflow-x-scroll overflow-x-padding"></div>
 
-    <br />
-    <br />
+      <h3 className="clearfix">By Coin</h3>
+      <div className="by-coin-table overflow-x-scroll"></div>
 
-    <h2>Methodology</h2>
+      <br />
+      <br />
 
-    <p>We take pictures of coins we find on the ground (either in situ or once they've been picked up) and then
-    message the pictures to a group chat with all of us. Almost all change is fair game, but we don't allow fishing
-    in fountains (deliberately left change).</p>
+      <h2>Methodology</h2>
 
-    <p>Each year, I manually scroll through our message history and log the results in a spreadsheet. I lightly
-    process and turn that spreadsheet into a CSV used to power these visualizations. Most of the time we note what
-    coins we find (such as "a scuffed penny" or "a dime and a nickel") but sometimes we forget, so I have to take a
-    guess (a blurry dime and nickel with no reference are hard to tell apart).</p>
+      <p>
+        We take pictures of coins we find on the ground (either in situ or once they've been picked
+        up) and then message the pictures to a group chat with all of us. Almost all change is fair
+        game, but we don't allow fishing in fountains (deliberately left change).
+      </p>
 
-    <p>Feel free to <a href="https://github.com/zachmargolis/pennies">check out the source on GitHub!</a></p>
-  </article>;
+      <p>
+        Each year, I manually scroll through our message history and log the results in a
+        spreadsheet. I lightly process and turn that spreadsheet into a CSV used to power these
+        visualizations. Most of the time we note what coins we find (such as "a scuffed penny" or "a
+        dime and a nickel") but sometimes we forget, so I have to take a guess (a blurry dime and
+        nickel with no reference are hard to tell apart).
+      </p>
+
+      <p>
+        Feel free to{" "}
+        <a href="https://github.com/zachmargolis/pennies">check out the source on GitHub!</a>
+      </p>
+    </article>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './stylesheets/styles.css'
+import { render } from 'preact';
+import { App } from './app.tsx';
+import './stylesheets/styles.css';
 
-render(<App />, document.getElementById('app')!)
+render(<App />, document.getElementById('app')!);
