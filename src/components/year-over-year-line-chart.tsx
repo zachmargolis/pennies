0c8ed604586b50e93f39e541ab99ce8b7ef4eb26
Zach Margolis <zbmargolis@gmail.com>
2023-12-18 18:00:09 -0800

    try force simulation for labels

--- src/components/year-over-year-line-chart.tsx
+++ src/components/year-over-year-line-chart.tsx
@@ -1,8 +1,14 @@
-import { useContext } from "preact/hooks";
+import { useContext, useMemo } from "preact/hooks";
 import { extent as d3Extent, max as d3Max } from "d3-array";
 import { axisBottom as d3AxisBottom, axisRight as d3AxisRight } from "d3-axis";
 import { scaleLinear as d3ScaleLinear } from "d3-scale";
 import { line as d3Line, curveMonotoneX as d3CurveMonotoneX } from "d3-shape";
+import {
+  forceSimulation as d3ForceSimulation,
+  forceCollide as d3ForceCollide,
+  forceX as d3ForceX,
+  forceY as d3ForceY,
+} from "d3-force";
 import { Row } from "../data";
 import Axis from "./axis";
 import { PLAIN_NUMBER_FORMAT } from "../formats";
@@ -41,6 +47,41 @@ export function YearOverYearLineChart() {
     .y(([_year, rows]) => y(rows.length))
     .curve(d3CurveMonotoneX);
 
+  interface Label {
+    person: string;
+    numCoins: number;
+    year: number;
+    x?: number;
+    y?: number;
+  }
+
+  const labelsByPerson = useMemo(() => {
+    const nodes: Label[] = Array.from(byPersonByYear.entries()).map(([person, personByYear]) => {
+      const [year, coins] = Array.from(personByYear.entries())[0];
+      return {
+        person,
+        numCoins: coins.length,
+        year,
+      };
+    });
+
+    const simulation = d3ForceSimulation(nodes)
+      .force("x", d3ForceX((d: Label) => x(d.year)).strength(1))
+      .force("y", d3ForceY((d: Label) => y(d.numCoins)).strength(1))
+      .force("collide", d3ForceCollide(10))
+      .stop();
+
+    for (let tick = 0; tick < 120; ++tick) {
+      simulation.tick();
+    }
+
+    const labels: Record<string, Label> = {};
+    nodes.forEach((node) => {
+      labels[node.person] = node;
+    });
+    return labels;
+  }, [byPersonByYear]);
+
   return (
     <div>
       <svg width={width} height={height}>
@@ -54,15 +95,7 @@ export function YearOverYearLineChart() {
         />
         <g transform={translate(padding.left, padding.top)}>
           {Array.from(byPersonByYear.entries()).map(([person, personByYear]) => {
-            const [firstYear, firstCoins] = Array.from(personByYear.entries())[0];
-
-            const firstLabelOffset: Record<string, number> = {
-              Mom: 5,
-              Brian: -5,
-              Dominica: -2,
-              Meghan: 3,
-            };
-
+            const label = labelsByPerson[person];
             return (
               <g data-person={person}>
                 {Array.from(personByYear).map(([year, coins]) => (
@@ -77,10 +110,7 @@ export function YearOverYearLineChart() {
                 <path className="line" stroke={color(person)} d={line(personByYear) || ""} />
                 <text
                   className="start-label"
-                  transform={translate(
-                    x(firstYear) - axisMargin,
-                    y(firstCoins.length) + (firstLabelOffset[person] || 0)
-                  )}
+                  transform={translate((label.x ?? 0) - axisMargin, label.y ?? 0)}
                 >
                   {person}
                 </text>
